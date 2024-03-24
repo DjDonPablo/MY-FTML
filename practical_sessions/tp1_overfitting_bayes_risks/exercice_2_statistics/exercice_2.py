b59a4b9faa75ebeeb85f73729743075d65ca8d4f
Maël Reynaud <78035043+DjDonPablo@users.noreply.github.com>
2024-03-24 12:33:51 +0100

    feat: TP1 done

--- practical_sessions/tp1_overfitting_bayes_risks/exercice_2_statistics/exercice_2.py
+++ practical_sessions/tp1_overfitting_bayes_risks/exercice_2_statistics/exercice_2.py
@@ -8,14 +8,13 @@ import matplotlib.pyplot as plt
 import os
 from sklearn.linear_model import LinearRegression
 
-
 def main():
     X_train = np.load(os.path.join("data", "X_train.npy"))
     X_test = np.load(os.path.join("data", "X_test.npy"))
     y_train = np.load(os.path.join("data", "y_train.npy"))
     y_test = np.load(os.path.join("data", "y_test.npy"))
 
-    rng = np.random.default_rng()
+    # rng = np.random.default_rng()
 
     """
     Study the variance of the test error estimation
@@ -23,11 +22,35 @@ def main():
     n_train will also have an influence on the result
     """
 
+    # idx = np.random.choice(np.arange(len(X_train)), 300, replace=False)
+    # reg = LinearRegression().fit(X_train[idx], y_train[idx])
+  
+    # std = []
+    # n_test_list = np.arange(10, len(X_test), 10)
+    # for i in n_test_list:
+    #     scores = []
+    #     for _ in range(150):
+    #         idx_2 = np.random.choice(np.arange(len(X_train)), i, replace=False)
+    #         scores.append(reg.score(X_test[idx_2], y_test[idx_2]))
+    #     std.append(np.array(scores).std())
+        
+    # plt.plot(n_test_list, std, "o", alpha=0.7)
+    # plt.show()
+
     """
     Study overfitting
     by subsampling the train set
     """
 
+    n_test_list = np.arange(10, 500, 10)
+    scores = []
+    for i in n_test_list:
+        idx = np.random.choice(np.arange(len(X_train)), i, replace=False)
+        reg = LinearRegression().fit(X_train[idx], y_train[idx])
+        scores.append(reg.score(X_train[idx], y_train[idx]) - reg.score(X_test, y_test))
+    plt.plot(n_test_list, scores, "o", alpha=0.7)
+    plt.yscale("log")
+    plt.show()
 
 if __name__ == "__main__":
     main()
